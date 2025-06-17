2213c8fc73c8d12128b04f08e73411f18e932c91
dungmadridista <phuonganhh323@gmail.com>
2025-06-17 20:59:59 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    question: "Tính chất nào không phải của lập trình hướng đối tượng?",
+    options: ["Đóng gói", "Kế thừa", "Đa hình", "Đánh địa chỉ"],
+    answer: 3
+  },
+  {
+    question: "Hàm tạo (constructor) trong C++ được gọi khi nào?",
+    options: ["Khi biên dịch", "Khi chương trình kết thúc", "Khi đối tượng được tạo", "Khi đối tượng bị hủy"],
+    answer: 2
+  },
+  {
+    question: "Từ khóa nào dùng để kế thừa trong C++?",
+    options: ["inherit", "class", "extends", "public"],
+    answer: 3
+  },
+  {
+    question: "Tính đa hình là gì?",
+    options: ["Khả năng giấu dữ liệu", "Khả năng gọi hàm giống tên với hành vi khác nhau", "Tạo ra lớp mới", "Tạo đối tượng mới"],
+    answer: 1
+  },
+  {
+    question: "Toán tử nào có thể được nạp chồng?",
+    options: ["::", ".", "?", "+"],
+    answer: 3
+  }
+];
